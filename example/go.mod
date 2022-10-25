304c2d3b740f616d84c5afa7636f2937c99d8386
Marcos Alvarez <marcos.alvarez@mobotix.com>
2022-10-25 12:55:22 +0200

    Implemented http interface w Reader and Writer

--- example/go.mod
+++ example/go.mod
@@ -1,3 +1,3 @@
-module example.com/m
+module example_http.com/m
 
 go 1.18
--- example/main.go
+++ example/main.go
@@ -1,31 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
-type bot interface {
-	getGreeting() string
-}
-type englishBot struct {}
-type spanishBot struct {}
-
-
-func (englishBot) getGreeting() string {
-	return "Hi!"
-}
-
-func (spanishBot) getGreeting() string {
-	return "Hola!"
-}
-
-func printGreating(b bot) {
-	fmt.Println(b.getGreeting())
-
-}
+type logWriter struct {}
 
 func main() {
-	var sb spanishBot
-	var eb englishBot
-	
-	printGreating(sb)
-	printGreating(eb)
+	resp, err := http.Get("http://www.google.com")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	// bs := make([]byte, 65535)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	//io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+// Implements Writer interface now
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Wrote", len(bs), "bytes")
+	return 1, nil
 }
