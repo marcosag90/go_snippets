69df25b33509bcfd89fb9960fc749b351786e611
Marcos Alvarez <marcos.alvarez@mobotix.com>
2022-10-25 18:29:39 +0200

    Develop basic hello world UI with Fyne

--- example/main.go
+++ example/main.go
@@ -2,35 +2,39 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	// "fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
-
+type App struct {
+	output *widget.Label
+}
 func main() {
-	links := []string {
-		"http://www.google.com",
-		"http://www.facebook.com",
-		"http://www.stackoverflow.com",
-		"http://www.amazon.com",
-	}
-	c := make(chan string)
-	for _, link := range links {
-		go checkLink(link, c)
-	}
+	a := app.New()
+	w := a.NewWindow("Hello World")
 
-	for l := range c {
-		go checkLink(l, c)
-	}
-	fmt.Println(<-c)
+	var myApp App
+	output, entry, btn := myApp.makeUI()
+
+	w.SetContent(container.NewVBox(output, entry, btn))
+	w.ShowAndRun()
+	cleanup()
 }
 
-func checkLink( link string, c chan string) {
-	_, err := http.Get(link)
-	if err != nil {
-		fmt.Println(link, "may be down")
-		c <- link
-		return
-	}
-	c <- link
-	fmt.Println(link, "is up")
+func (app *App) makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
+	output := widget.NewLabel("Hello World")
+	entry := widget.NewEntry()
+	btn := widget.NewButton("Enter", func() {
+		app.output.SetText(entry.Text)
+	})
+	btn.Importance = widget.MediumImportance
+
+	app.output = output
+	return output, entry, btn
+}
+
+func cleanup() {
+	fmt.Println("cleaning stuff up")
 }
